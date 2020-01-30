4b0400e4e4768dfa546f0367beae32f8752f750c
sanjib biswas <sanjib_biswas2002@macbook-pro.attlocal.net>
2020-01-29 23:41:50 -0600

    make it better

---
+++ build/precache-manifest.b9c77b415c1a16e7129aeadc90d36f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b2378a4c06535aab3eeb438a29c6ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f380eaf05ae2b58a14b2",
+    "url": "/static/css/main.619c8c13.chunk.css"
+  },
+  {
+    "revision": "6a9a570bb7e8f376f308",
+    "url": "/static/js/2.55431740.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.55431740.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f380eaf05ae2b58a14b2",
+    "url": "/static/js/main.956a14a4.chunk.js"
+  },
+  {
+    "revision": "e0648a0a5c10da4459d6",
+    "url": "/static/js/runtime-main.d6e9764d.js"
+  }
+]);
